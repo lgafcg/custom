d424d0cf295e4c74d8ce28b7831d17e107ad81d8
lgafcg <49931100+lgafcg@users.noreply.github.com>
2019-04-30 18:34:46 +0600

    Update extension.js

--- extension.js
+++ extension.js
@@ -42,8 +42,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
         botName: 'lgafcg',
         language: 'russian',
-        chatLink: 'https://rawgit.com/lgafcg/source/master/lang/en.json',
-        scriptLink: 'https://rawgit.com/lgafcg/source/master/basicBot.js',
+        chatLink: 'https://raw.githack.com/lgafcg/source/master/lang/en.json',
+        scriptLink: 'https://raw.githack.com/lgafcg/source/master/basicBot.js',
         roomLock: false, // Requires an extension to re-load the script
         startupCap: 1, // 1-200
         startupVolume: 0, // 0-100
@@ -102,13 +102,13 @@
         songstats: true,
         commandLiteral: '!',
         blacklists: {
-            NSFW: 'https://rawgit.com/lgafcg/custom/master/blacklists/NSFWlist.json',
-            OP: 'https://rawgit.com/lgafcg/custom/master/blacklists/OPlist.json',
-            BANNED: 'https://rawgit.com/lgafcg/custom/master/blacklists/BANNEDlist.json'
+            NSFW: 'https://raw.githack.com/lgafcg/custom/master/blacklists/NSFWlist.json',
+            OP: 'https://raw.githack.com/lgafcg/custom/master/blacklists/OPlist.json',
+            BANNED: 'https://raw.githack.com/lgafcg/custom/master/blacklists/BANNEDlist.json'
         }
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/lgafcg/source/master/basicBot.js", extend);
+    $.getScript("https://raw.githack.com/lgafcg/source/master/basicBot.js", extend);
 
 }).call(this);
